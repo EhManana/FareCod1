43eb57572393af659ebd508b2c837b3880b04551
ehmanana <chafej@wit.edu>
2019-03-26 20:02:29 -0400

    Basic pages and buttons

---
+++ app/src/main/java/com/example/farecod/transferGiveOrTake.java
@@ -0,0 +1,13 @@
+package com.example.farecod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class transferGiveOrTake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer_give_or_take);
+    }
+}
